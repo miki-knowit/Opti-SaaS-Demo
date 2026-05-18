54bb9b04fe9168fd62532e3b332620b3aa94f51d
Miki Lamberg <miki.lamberg@knowit.se>
2026-05-18 12:46:31 +0200

    Trim redundant code from ImageBlock (boolean is used in TextImageBlock), add base styling to certain elements

--- app/components/blocks/ImageBlock.vue
+++ app/components/blocks/ImageBlock.vue
@@ -8,41 +8,18 @@ defineProps<{
                 default?: string | null;
             } | null;
         } | null;
-        PlaceImageRight?: boolean; // named PlaceImageRight in the CMS
     };
 }>();
 </script>
 
 <template>
-    <div class="image-block">
-        <div
-            class="image-block__content"
-            :class="{ 'image-block__content--align-top': imageBlock.PlaceImageRight }"
-        >
-            <figure v-if="imageBlock.ImageProp?.url?.default" class="image-block__image-container">
-                <figcaption v-if="imageBlock.HeadingProp" class="image-block__caption">
-                    {{ imageBlock.HeadingProp }}
-                </figcaption>
-                <img :src="imageBlock.ImageProp.url.default" :alt="imageBlock.HeadingProp || ''" />
-            </figure>
-        </div>
-    </div>
+    <figure v-if="imageBlock.ImageProp?.url?.default" class="image-block">
+        <figcaption v-if="imageBlock.HeadingProp" class="image-block__caption">
+            {{ imageBlock.HeadingProp }}
+        </figcaption>
+        <img
+            :src="imageBlock.ImageProp.url.default"
+            :alt="imageBlock.HeadingProp || 'Image not available.'"
+        />
+    </figure>
 </template>
-
-<style scoped>
-.image-block {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-.image-block__content {
-    width: 100%;
-    max-width: 200px;
-    max-height: 200px;
-}
-
-.image-block__content--align-top {
-    align-self: flex-start;
-}
-</style>
